1ebf78454d9df5390f6cb16b78721014fc5f766e
Nikolai Kozheurov <kozheurovnikolay@mail.ru>
2022-12-13 22:27:24 +0200

    some changes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println(new Date());
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        System.out.println(simpleDateFormat.format(new Date()));
     }
 }
